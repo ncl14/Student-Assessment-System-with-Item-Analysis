1b68626e0e3fbc7b501c70b345e5583fd22dafb7
bryll <bryll@DESKTOP-4NCN539>
2025-12-16 00:22:20 +0800

    section

--- Forms/SubjectManagement/SectionManagementForm.Designer.cs
+++ Forms/SubjectManagement/SectionManagementForm.Designer.cs
@@ -28,19 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblSectionName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.lblCourse = new System.Windows.Forms.Label();
+            this.lblAssignedTeacher = new System.Windows.Forms.Label();
+            this.cmbSec = new System.Windows.Forms.ComboBox();
+            this.cmbSub = new System.Windows.Forms.ComboBox();
+            this.cmbCourse = new System.Windows.Forms.ComboBox();
+            this.cmbAssigned = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // lblSectionName
+            // 
+            this.lblSectionName.AutoSize = true;
+            this.lblSectionName.BackColor = System.Drawing.Color.SteelBlue;
+            this.lblSectionName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSectionName.Location = new System.Drawing.Point(28, 100);
+            this.lblSectionName.Name = "lblSectionName";
+            this.lblSectionName.Size = new System.Drawing.Size(125, 21);
+            this.lblSectionName.TabIndex = 0;
+            this.lblSectionName.Text = "Section Name :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.SteelBlue;
+            this.textBox1.Location = new System.Drawing.Point(-1, -2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 454);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.BackColor = System.Drawing.Color.SteelBlue;
+            this.lblSubject.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubject.Location = new System.Drawing.Point(28, 172);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(75, 21);
+            this.lblSubject.TabIndex = 2;
+            this.lblSubject.Text = "Subject :";
+            // 
+            // lblCourse
+            // 
+            this.lblCourse.AutoSize = true;
+            this.lblCourse.BackColor = System.Drawing.Color.SteelBlue;
+            this.lblCourse.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCourse.Location = new System.Drawing.Point(28, 243);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(70, 21);
+            this.lblCourse.TabIndex = 3;
+            this.lblCourse.Text = "Course :";
+            // 
+            // lblAssignedTeacher
+            // 
+            this.lblAssignedTeacher.AutoSize = true;
+            this.lblAssignedTeacher.BackColor = System.Drawing.Color.SteelBlue;
+            this.lblAssignedTeacher.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAssignedTeacher.Location = new System.Drawing.Point(28, 315);
+            this.lblAssignedTeacher.Name = "lblAssignedTeacher";
+            this.lblAssignedTeacher.Size = new System.Drawing.Size(148, 21);
+            this.lblAssignedTeacher.TabIndex = 4;
+            this.lblAssignedTeacher.Text = "Assigned teacher :";
+            // 
+            // cmbSec
+            // 
+            this.cmbSec.FormattingEnabled = true;
+            this.cmbSec.Location = new System.Drawing.Point(198, 100);
+            this.cmbSec.Name = "cmbSec";
+            this.cmbSec.Size = new System.Drawing.Size(337, 21);
+            this.cmbSec.TabIndex = 5;
+            // 
+            // cmbSub
+            // 
+            this.cmbSub.FormattingEnabled = true;
+            this.cmbSub.Location = new System.Drawing.Point(198, 172);
+            this.cmbSub.Name = "cmbSub";
+            this.cmbSub.Size = new System.Drawing.Size(337, 21);
+            this.cmbSub.TabIndex = 6;
+            // 
+            // cmbCourse
+            // 
+            this.cmbCourse.FormattingEnabled = true;
+            this.cmbCourse.Location = new System.Drawing.Point(198, 243);
+            this.cmbCourse.Name = "cmbCourse";
+            this.cmbCourse.Size = new System.Drawing.Size(337, 21);
+            this.cmbCourse.TabIndex = 7;
+            // 
+            // cmbAssigned
+            // 
+            this.cmbAssigned.FormattingEnabled = true;
+            this.cmbAssigned.Location = new System.Drawing.Point(198, 315);
+            this.cmbAssigned.Name = "cmbAssigned";
+            this.cmbAssigned.Size = new System.Drawing.Size(337, 21);
+            this.cmbAssigned.TabIndex = 8;
             // 
             // SectionManagementForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbAssigned);
+            this.Controls.Add(this.cmbCourse);
+            this.Controls.Add(this.cmbSub);
+            this.Controls.Add(this.cmbSec);
+            this.Controls.Add(this.lblAssignedTeacher);
+            this.Controls.Add(this.lblCourse);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.lblSectionName);
+            this.Controls.Add(this.textBox1);
             this.Name = "SectionManagementForm";
             this.Text = "SectionManagementForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblSectionName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.Label lblCourse;
+        private System.Windows.Forms.Label lblAssignedTeacher;
+        private System.Windows.Forms.ComboBox cmbSec;
+        private System.Windows.Forms.ComboBox cmbSub;
+        private System.Windows.Forms.ComboBox cmbCourse;
+        private System.Windows.Forms.ComboBox cmbAssigned;
     }
 }
